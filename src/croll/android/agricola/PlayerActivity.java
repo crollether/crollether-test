84cdddbe35797e2098fb12033c568a916dbf8440
andrew <andrew159@gmail.com>
2011-10-09 16:06:22 +0100

    Added brown border around fields and fields now have correct "selected" behaviour

--- src/croll/android/agricola/PlayerActivity.java
+++ src/croll/android/agricola/PlayerActivity.java
@@ -1,5 +1,8 @@
 package croll.android.agricola;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.LightingColorFilter;
@@ -16,7 +19,9 @@ public class PlayerActivity extends Activity {
 	private int fieldPoints_ = -1;
 	private int totalPoints_ = 0;
 	TextView fieldPointText_, totalPointsText_;
-	  
+	Map<Integer, Button> fieldButtonMap = new HashMap<Integer, Button>();
+	Drawable unselectedField_, selectedField_;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -25,6 +30,14 @@ public class PlayerActivity extends Activity {
 		
 		fieldPointText_ = (TextView) findViewById(R.id.fieldPoints);
 		totalPointsText_ = (TextView) findViewById(R.id.total_points);
+		fieldButtonMap.put(R.id.field_points_01, (Button) findViewById(R.id.field_points_01));
+		fieldButtonMap.put(R.id.field_points_2,  (Button) findViewById(R.id.field_points_2));
+		fieldButtonMap.put(R.id.field_points_3,  (Button) findViewById(R.id.field_points_3));
+		fieldButtonMap.put(R.id.field_points_4,  (Button) findViewById(R.id.field_points_4));
+		fieldButtonMap.put(R.id.field_points_5,  (Button) findViewById(R.id.field_points_5));
+		
+		unselectedField_ = this.getResources().getDrawable(R.drawable.field);
+		selectedField_ = this.getResources().getDrawable(R.drawable.field_selected);
 		
 		updateFieldPointText(fieldPoints_);
 		
@@ -33,36 +46,32 @@ public class PlayerActivity extends Activity {
 
 	public void onClickHandler(View view) {
 		Button button;
- 		switch (view.getId()) {
+		int buttonId = view.getId();
+ 		switch (buttonId) {
  		case R.id.field_points_01:
  			fieldPoints_ = -1;
  			updateFieldPointText(fieldPoints_);
- 			button = (Button) findViewById(R.id.field_points_01);
- 			button.setBackgroundDrawable(R.drawable.field_selected);
+ 			updateSelectedField(buttonId);
  			break;
  		case R.id.field_points_2:
  			fieldPoints_ = 1;
  			updateFieldPointText(fieldPoints_);
- 			button = (Button) findViewById(R.id.field_points_2);
- 			button.setBackgroundDrawable(R.drawable.field_selected);
+ 			updateSelectedField(buttonId);
  			break;
  		case R.id.field_points_3:
  			fieldPoints_ = 2;
  			updateFieldPointText(fieldPoints_);
- 			button = (Button) findViewById(R.id.field_points_3);
- 			button.setBackgroundDrawable(R.drawable.field_selected);
+ 			updateSelectedField(buttonId);
  			break;
  		case R.id.field_points_4:
  			fieldPoints_ = 3;
  			updateFieldPointText(fieldPoints_);
- 			button = (Button) findViewById(R.id.field_points_4);
- 			button.setBackgroundDrawable(R.drawable.field_selected);
+ 			updateSelectedField(buttonId);
  			break;
  		case R.id.field_points_5:
  			fieldPoints_ = 4;
  			updateFieldPointText(fieldPoints_);
- 			button = (Button) findViewById(R.id.field_points_5);
- 			button.setBackgroundDrawable(R.drawable.field_selected);
+ 			updateSelectedField(buttonId);
  			break;
  		}
  		updateTotalPoints();
@@ -73,6 +82,16 @@ public class PlayerActivity extends Activity {
 		String fieldPointsString = "Field points: ";
 		fieldPointsString = fieldPointsString.concat(String.valueOf(fieldPoints));
 		fieldPointText_.setText(fieldPointsString);
+	}
+	
+	private void updateSelectedField(int fieldId)
+	{
+		//First reset all butons to unselected
+		for (Map.Entry<Integer, Button> e : fieldButtonMap.entrySet())
+		    e.getValue().setBackgroundDrawable(unselectedField_);
+
+		// Then set the passed in button as the selected field
+		fieldButtonMap.get(fieldId).setBackgroundDrawable(selectedField_);
 	}
 	
 	private void updateTotalPoints()
